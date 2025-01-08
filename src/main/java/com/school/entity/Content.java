f7715723d6950c2ebb1b65e5059fd5b79d64e224
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2025-01-08 11:02:59 +0800

    Add getters and setters to `Content`

--- src/main/java/com/school/entity/Content.java
+++ src/main/java/com/school/entity/Content.java
@@ -1,13 +1,18 @@
 package com.school.entity;
 
+import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 import com.school.constants.ContentCategory;
 
@@ -21,7 +26,7 @@ public class Content {
     @Column(nullable = false)
     private String title;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "author_id", nullable = false)
     private User author;
 
@@ -30,4 +35,56 @@ public class Content {
 
     @Column(name = "video_id", nullable = false)
     private String videoId;
+
+    @Column(name = "uploaded_date", nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date uploadedDate;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    public ContentCategory getCategory() {
+        return category;
+    }
+
+    public void setCategory(ContentCategory category) {
+        this.category = category;
+    }
+
+    public String getVideoId() {
+        return videoId;
+    }
+
+    public void setVideoId(String videoId) {
+        this.videoId = videoId;
+    }
+
+    public Date getUploadedDate() {
+        return uploadedDate;
+    }
+
+    public void setUploadedDate(Date uploadedDate) {
+        this.uploadedDate = uploadedDate;
+    }
 }
